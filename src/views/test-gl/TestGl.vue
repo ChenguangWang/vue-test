eb8d1c871373ffe96f1912c0bb0e7ac8d0a63348
Wang Chenguang <chenguang.wang@tendcloud.com>
2022-02-22 17:38:15 +0800

    feat: gl添加代码注释

--- src/views/test-gl/TestGl.vue
+++ src/views/test-gl/TestGl.vue
@@ -43,14 +43,19 @@ export default {
       //创建画布
       const canvas = document.querySelector('canvas');
       const gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+      // 把图片上下对称翻转坐标轴
       gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+      // 创建并初始化顶点着色器对象
       const buffer = gl.createBuffer();
+      // 将创建的着色器对象buffer 绑定到 gl.ARRAY_BUFFER
       gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+      // 创建并初始化buffer对象的数据存储区
       gl.bufferData(
         gl.ARRAY_BUFFER,
         new Float32Array([-1, -1, -1, 4, 4, -1]), // see a-big-triangle
         gl.STATIC_DRAW
       );
+      // 设置视口 gl.viewport(x, y, width, height);
       gl.viewport(0, 0, canvas.width, canvas.height);
 
       //生成过渡需要的texture
@@ -86,7 +91,6 @@ export default {
 
           const total = delay + duration;
           const progress = Math.max(0, (timestamp - index * total * 1000 - delay * 1000) / (duration * 1000)); //0-1的数，其实就是百分比
-          //console.info(index,timestamp);
           //开始过渡
           transition.draw(progress, from, to, canvas.width, canvas.height, { persp: 1.5, unzoom: 0.6 });
         }
